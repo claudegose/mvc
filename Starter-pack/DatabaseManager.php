4c511e6e6524faf8bb1410366b9dafb75698944c
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-19 13:50:46 +0200

    Add DatabaseManager class

---
+++ Starter-pack/DatabaseManager.php
@@ -0,0 +1,32 @@
+<?php
+
+class DatabaseManager
+{
+    private $host;
+    private $user;
+    private $password;
+    private $dbname;
+    public $connection;
+
+    public function __construct(string $host, string $user, string $password, string $dbname)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->password = $password;
+        $this->dbname = $dbname;
+    }
+
+    public function connect(): PDO
+    {
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        $this->connection = new PDO($dsn, $this->user, $this->password);
+        $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        return $this->connection;
+    }
+}
+
+
+
+
+
